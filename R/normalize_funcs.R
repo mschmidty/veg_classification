023ff14636320d7b77354b36426e7162b25b7a9e
Mike Schmidt <mschmidty@gmail.com>
2020-03-19 09:21:04 -0600

    normalize changes

--- R/normalize_funcs.R
+++ R/normalize_funcs.R
@@ -23,18 +23,3 @@ rangify <- function(raster, parallel = FALSE){
   
   raster
 }
-
-source("R/load_funcs.R")
-
-
-imagery_path<-"DATA/SanMiguel_West/Tiffs/SM_GypsumGapNW_0103_180611.tif"
-r<-brick(imagery_path)
-r_norm<-rangify(r, 4)
-
-imagery_path<-"DATA/SanMiguel_West/Tiffs/SM_GypsumGapNW_0104_180611.tif"
-r<-brick(imagery_path)
-r_norm2<-rangify(r, 4)
-
-writeRaster(r_norm2, "output/normalize_test/NORMALIZED_SM_GypsumGapNW_0104_180611.tif")
-writeRaster(r_norm, "output/normalize_test/NORMALIZED_SM_GypsumGapNW_0103_180611.tif")
-
